3c74fadaac3989f0964b9b887df377267ee598e3
Moazam05 <salmanmoazam08@gmail.com>
2022-05-27 22:04:28 +0500

    search functionality

--- src/components/Todo/AllTodo.js
+++ src/components/Todo/AllTodo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AllTodo.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -7,6 +7,8 @@ import { deleteTodo } from '../../features/todoSlice';
 const AllTodo = () => {
   const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos);
+
+  const [searchTodo, setSearchTodo] = useState('');
 
   const deleteAgenda = (id) => {
     dispatch(deleteTodo({ id }));
@@ -28,6 +30,14 @@ const AllTodo = () => {
                     />
                     <h2 className='my-4'>Task List</h2>
                   </div>
+                  <div className='col-sm-4'>
+                    <input
+                      type='text'
+                      className='form-control mb-4'
+                      placeholder='Search Tod'
+                      onChange={(e) => setSearchTodo(e.target.value)}
+                    />
+                  </div>
                   <table className='table text-white mb-0'>
                     <thead>
                       <tr>
@@ -38,59 +48,74 @@ const AllTodo = () => {
                       </tr>
                     </thead>
                     <tbody>
-                      {todos.map((todo, index) => {
-                        const { title, task, priority } = todo;
-                        return (
-                          <tr className='fw-normal' key={index}>
-                            <th>
-                              <img
-                                src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp'
-                                alt='avatar 1'
-                                style={{ width: '45px', height: 'auto' }}
-                              />
-                              <span className='ms-2'>{title}</span>
-                            </th>
-                            <td className='align-middle'>
-                              <span>{task}</span>
-                            </td>
-                            <td className='align-middle'>
-                              <h6 className='mb-0'>
-                                <span
-                                  className={
-                                    priority === 'High'
-                                      ? 'badge bg-danger'
-                                      : priority === 'Medium'
-                                      ? 'badge bg-success'
-                                      : 'badge bg-warning'
-                                  }
+                      {todos
+                        .filter((item) => {
+                          if (searchTodo === '') {
+                            return item;
+                          } else if (
+                            item.title
+                              .toLowerCase()
+                              .includes(searchTodo.toLowerCase())
+                          )
+                            return item;
+                        })
+                        .map((todo, index) => {
+                          const { title, task, priority } = todo;
+                          return (
+                            <tr className='fw-normal' key={index}>
+                              <th>
+                                <img
+                                  src='https://random.imagecdn.app/500/150'
+                                  alt='avatar 1'
+                                  style={{
+                                    width: '30px',
+                                    height: '30px',
+                                    borderRadius: '50%',
+                                  }}
+                                />
+                                <span className='ms-2'>{title}</span>
+                              </th>
+                              <td className='align-middle'>
+                                <span>{task}</span>
+                              </td>
+                              <td className='align-middle'>
+                                <h6 className='mb-0'>
+                                  <span
+                                    className={
+                                      priority === 'High'
+                                        ? 'badge bg-danger'
+                                        : priority === 'Medium'
+                                        ? 'badge bg-success'
+                                        : 'badge bg-warning'
+                                    }
+                                  >
+                                    {`${priority} Priority`}
+                                  </span>
+                                </h6>
+                              </td>
+                              <td className='align-middle'>
+                                <Link
+                                  to={`update-todo/${todo.id}`}
+                                  data-mdb-toggle='tooltip'
+                                  title='Update'
                                 >
-                                  {`${priority} Priority`}
-                                </span>
-                              </h6>
-                            </td>
-                            <td className='align-middle'>
-                              <Link
-                                to={`update-todo/${todo.id}`}
-                                data-mdb-toggle='tooltip'
-                                title='Update'
-                              >
-                                <i className='fas fa-edit fa-lg text-success me-3' />
-                              </Link>
-                              <button
-                                style={{
-                                  background: 'transparent',
-                                  border: 'none',
-                                }}
-                                onClick={() => deleteAgenda(todo.id)}
-                                data-mdb-toggle='tooltip'
-                                title='Remove'
-                              >
-                                <i className='fas fa-trash-alt fa-lg text-warning' />
-                              </button>
-                            </td>
-                          </tr>
-                        );
-                      })}
+                                  <i className='fas fa-edit fa-lg text-success me-3' />
+                                </Link>
+                                <button
+                                  style={{
+                                    background: 'transparent',
+                                    border: 'none',
+                                  }}
+                                  onClick={() => deleteAgenda(todo.id)}
+                                  data-mdb-toggle='tooltip'
+                                  title='Remove'
+                                >
+                                  <i className='fas fa-trash-alt fa-lg text-warning' />
+                                </button>
+                              </td>
+                            </tr>
+                          );
+                        })}
                     </tbody>
                   </table>
                 </div>
